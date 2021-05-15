a4e8a1ddb87dbd51e145b4fabf0130ab5544132b
Dmytro-Sk <0971898920d@gmail.com>
2021-05-15 13:37:25 +0300

    Add models to an app

--- djangoproject_site/settings.py
+++ djangoproject_site/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls'
+    'apps.polls',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
